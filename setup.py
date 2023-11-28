8d96e4ef0bf9640c12678f265fe16efdeaf38f03
Caleb John <cajohn0205@gmail.com>
2023-11-27 23:03:41 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pongo-python',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     description='A Python package for interacting with the Pongo API',
     long_description=open('README.md').read(),
